0f97867c043a6730e2eb0d021f6dcffc5d245b27
manish-dev3490 <kmanish87064@gmail.com>
2024-06-03 12:36:52 +0530

    implemented 2 concepts

--- Basics_of_CPP/introduction.cpp
+++ Basics_of_CPP/introduction.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-int main() {
-    std::cout << "hello ji";
-    
-    return 0; 
+int main()
+{
+
+    // Calculate the power of a number
+    // int number, power, answer;
+    // cout<<"Enter the number";
+    // cin >> number;
+
+    // cout << "Enter the power";
+    // cin >> power;
+    // answer = number;
+
+    // for (int i = 1; i < power; i++)
+    // {
+    //     answer = number * answer;
+    // }
+
+    // cout << "Answer is " << answer;
+    // return 0;
+
+
+
+
+    // Check wheather number is prime or not
+    // int number;
+    // cout << "Enter the number you want to check";
+    // cin >> number;
+
+    // if (number < 2)
+    // {
+    //     cout << "number is less than two it is not prime ";
+    //     return 0;
+    // }
+
+    // else
+    // {
+    //     for (int i = 2; i < number - 1; i++)
+    //     {
+    //         if (number % i == 0)
+    //             cout << "This is not Prime";
+    //     }
+
+    //     cout << "Prime Nummber";
+    // }
 }
