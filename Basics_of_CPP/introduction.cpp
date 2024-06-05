251e686d8d00ce0ff5679c3bc52eeed0d680f976
manish-dev3490 <kmanish87064@gmail.com>
2024-06-05 14:29:57 +0530

    commented whole code

--- Basics_of_CPP/introduction.cpp
+++ Basics_of_CPP/introduction.cpp
@@ -139,11 +139,11 @@ int main()
     // }
 
 
-    for (int row=5; row>=1;row--){
-        for(int col=row;col>=1;col--){
-            cout<<"*"<<" ";
-        }
+    // for (int row=5; row>=1;row--){
+    //     for(int col=row;col>=1;col--){
+    //         cout<<"*"<<" ";
+    //     }
 
-        cout<<endl;
-    }
+    //     cout<<endl;
+    // }
 }
