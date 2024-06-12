77b49df1bdfcdf90eaa9feea9fffc0e08c0ce97f
manish-dev3490 <kmanish87064@gmail.com>
2024-06-12 18:35:33 +0530

    solved questions

---
+++ Basics_of_CPP/Leetcode.cpp
@@ -0,0 +1,51 @@
+// In this file i am doing some leetcode questios ractice
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // Add Digit Of the number Question 1
+    // int num;
+    // cout << "Enter the number you want to add digitis for  ";
+    // cin >> num;
+    // while (num > 9)
+    // {
+    // int ans = 0, rem;
+
+    //     while (num != 0)
+    //     {
+    //         rem = num % 10
+
+    //         num /= 10;
+    //         ans = rem + ans;
+    //     }
+    //     num = ans;
+    // }
+    // cout << num;
+
+
+
+    // Power of two
+    // int n;
+    // int devide;
+    // int answer=1;
+    // cout << "Check the number you want to see if it comes inside 2 ki power";
+    // cin >> n;
+
+    // if (n < 2)
+    // {
+    //     return 0;
+    // }
+
+    // else if(n>=2){
+    //    devide=n/2;
+    //     for(int i=1;i<=devide;i++){
+    //         answer=answer*2;
+    //         if(n==answer){
+    //             cout<<"Yes this number" <<n <<"lies in 2 ki power";
+    //             return 1;
+    //         } 
+    //     }
+    //     cout<<"his number does not lies in 2 ki power";
+    // }
+}
