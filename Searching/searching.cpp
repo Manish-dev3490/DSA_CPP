05cb7c84792506cc0bc5cb9e128e01ac70ed54bd
manish-dev3490 <kmanish87064@gmail.com>
2024-07-08 10:40:48 +0530

    Binary search algorithum

---
+++ Searching/searching.cpp
@@ -0,0 +1,51 @@
+// In this file i am learning searching algorithum binary search
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+    int mid;
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        else if (arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[1000];
+    cout << "Enter the size of the array";
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key element in the array";
+    int key;
+    cin >> key;
+
+    cout << BinarySearch(arr, n, key);
+}
