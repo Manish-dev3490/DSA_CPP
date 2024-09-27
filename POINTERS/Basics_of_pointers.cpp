153af1b0be3459ea03b40b230c43d11b2aa60563
manish-dev3490 <kmanish87064@gmail.com>
2024-09-27 20:04:44 +0530

    some more

--- POINTERS/Basics_of_pointers.cpp
+++ POINTERS/Basics_of_pointers.cpp
@@ -24,28 +24,35 @@ int main(){
 //     cout<<endl;
 //    int i = 3;
 //    int *t = &i;
-   
-//    *t = *t +1;
-//     cout << *t << endl;
+  
 
 
 
 
 // Now we are playin g with ppointers through double data type
-    double num=10.10;
-    double *ptr=&num;
-    cout<<"The address of num is "<<&num<<endl;
-    cout<<"The address of num is "<<ptr<<endl;
-    cout<<"The value of num is "<<*ptr<<endl;
+    // double num=10.10;
+    // double *ptr=&num;
+    // cout<<"The address of num is "<<&num<<endl;
+    // cout<<"The address of num is "<<ptr<<endl;
+    // cout<<"The value of num is "<<*ptr<<endl;
 
 
     // playing with pointer arithmetic operators
-    cout<<(*ptr)++;
-    cout<<endl;
-    cout<<(*ptr)--;
-    cout<<endl;
-    cout<<(*ptr);
+    // cout<<(*ptr)++;
+    // cout<<endl;
+    // cout<<(*ptr)--;
+    // cout<<endl;
+    // cout<<(*ptr);
 
+
+
+// playing ointers with arrays just for better understanding of pointers
+int arr[10]={1,2,3,4,5,6,7,8,9,10};
+cout<<*arr+1;
+cout<<endl;
+cout<<*(arr+2);
+cout<<endl;
+cout<<arr+2;
 
 
 
