6044ec41b8b1b166f1aa58a4a5719547f7e40f3b
manish-dev3490 <kmanish87064@gmail.com>
2025-03-15 13:54:53 +0530

    converted array into  doubly linked list using recursion

--- Linked_List/DoublyLL.cpp
+++ Linked_List/DoublyLL.cpp
@@ -18,6 +18,23 @@ public:
     }
 };
 
+
+Node * ConvertLL(int arra[],int index,int size , Node *head,Node *tail){
+    if(index==size)return head;
+    if(head==NULL){
+        head=new Node(arra[index]);
+        tail=head;
+        return ConvertLL(arra,index+1,size,head,tail);
+    }
+
+    else {
+        Node *temp=new Node(arra[index]);
+        temp->prev=tail;
+        tail->next=temp;
+        tail=temp;
+        return ConvertLL(arra,index+1,size,head,tail);
+    }
+}
 int main()
 {
 
@@ -55,29 +72,39 @@ int main()
 
 
     // Convrt array into doubly linked listt 
-    int arra[5]={1,2,3,4,5};
+    // int arra[5]={1,2,3,4,5};
+    // Node *head=NULL,*tail=NULL;
+    // for(int i=0;i<5;i++){
+    //     if(head==NULL){
+    //         head=new Node(arra[i]);
+    //         tail=head;
+    //     }
+
+    //     else {
+    //         Node *temp=new Node(arra[i]);
+    //         temp->prev=tail;
+    //         tail->next=temp;
+    //         tail=temp;
+    //     }
+    // }
+
+
+    // Node *temp=head;
+    // while(temp!=NULL){
+    //     cout<<temp->data;
+    //     cout<<endl;
+    //     temp=temp->next;
+    // }
+
+    
+
+    // converting array into linked list using recusrion
+    int arr[4]={2,4,6,8};
     Node *head=NULL,*tail=NULL;
-    for(int i=0;i<5;i++){
-        if(head==NULL){
-            head=new Node(arra[i]);
-            tail=head;
-        }
-
-        else {
-            Node *temp=new Node(arra[i]);
-            temp->prev=tail;
-            tail->next=temp;
-            tail=temp;
-        }
+    Node *start=ConvertLL(arr,0,4,head,tail);
+    while(start!=NULL){
+        cout<<start->data<<endl;
+        start=start->next;
     }
-
-
-    Node *temp=head;
-    while(temp!=NULL){
-        cout<<temp->data;
-        cout<<endl;
-        temp=temp->next;
-    }
-
-
+    
 }
