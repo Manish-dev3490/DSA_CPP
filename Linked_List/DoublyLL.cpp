2fc7c4ee658663ac08891ec20dcc499ce72bc5cd
manish-dev3490 <kmanish87064@gmail.com>
2025-03-15 12:09:44 +0530

    insetion at the end of the linked list

--- Linked_List/DoublyLL.cpp
+++ Linked_List/DoublyLL.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Now i am goint to learn doubly linked list which is used to store the adrress of next and previus node let see with the exampless.......
+// Now i am goint to learn doubly linked list which is used to store the adrress of next and previus node let see with the exampless....... Insertion at start,insertion at end
 
 class Node
 {
@@ -23,17 +23,33 @@ int main()
 
     // Insertion at the start of the linked list ----->
 
-    Node *head = new Node(900);
+    // Node *head = new Node(900);
 
-    Node *temp = new Node(500);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    // Node *temp = new Node(500);
+    // temp->next = head;
+    // head->prev = temp;
+    // head = temp;
 
-    Node *iterate = head;
-    while (iterate != NULL)
-    {
-        cout << iterate->data << endl;
-        iterate = iterate->next;
-    }
+    // Node *iterate = head;
+    // while (iterate != NULL)
+    // {
+    //     cout << iterate->data << endl;
+    //     iterate = iterate->next;
+    // }
+
+
+
+
+
+    // Insertion at the end of the linked list 
+    // Node *head=new Node(10);
+    // Node *temp=new Node(20);
+    // head->next=temp;
+    // temp->prev=head;
+    // Node *tail=head;
+    // while(tail!=NULL){
+    //     cout<<tail->data<<endl;
+    //     tail=tail->next;
+    // }
+
 }
