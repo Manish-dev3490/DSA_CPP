4d58a1f2fe2c89dba93176633d5e6659dced91f5
manish-dev3490 <kmanish87064@gmail.com>
2025-06-03 07:54:53 +0530

    indentaiion fixes

--- Leetcode_Problems/Slidingwindow.cpp
+++ Leetcode_Problems/Slidingwindow.cpp
@@ -1,19 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int main(){
-    int arr[6]={3,6,2,7,8,11};
-    int k=3;
-    int count=0;
-    for(int i=0;i<6;i++){
-        int j=i;
-        while(j<6 && count!=k){
-            cout<<arr[j];
+int main()
+{
+    int arr[6] = {3, 6, 2, 7, 8, 11};
+    int k = 3;
+    int count = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        int j = i;
+        while (j < 6 && count != k)
+        {
+            cout << arr[j];
             count++;
             j++;
         }
-        cout<<endl;
-        count=0;
+        cout << endl;
+        count = 0;
     }
 }
+
