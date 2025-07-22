db9cf032847180ab6e6f95b2781218dd93429ed9
manish-dev3490 <kmanish87064@gmail.com>
2025-07-22 22:50:32 +0530

    written some description about prefix sums

--- DSAPatterns/PrefixSum/subarray-equal-k-sum.cpp
+++ DSAPatterns/PrefixSum/subarray-equal-k-sum.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 
 int main(){
+
+    // Prefix Sum Pattern is a technique which is used to count the sum of all elements in array and use that approach wise
+
+
     return 0;
 }
