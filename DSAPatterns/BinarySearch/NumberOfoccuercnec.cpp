b906c4291aa3e81b1ce548e9149d1559345c3172
manish-dev3490 <kmanish87064@gmail.com>
2025-08-08 15:06:17 +0530

    optimized approach usning the binaryy search

--- DSAPatterns/BinarySearch/NumberOfoccuercnec.cpp
+++ DSAPatterns/BinarySearch/NumberOfoccuercnec.cpp
@@ -5,25 +5,54 @@ using namespace std;
 class Solution
 {
 public:
-    int countFreq(vector<int> &arr, int target)
+    int countFreq(vector<int> &arr, int key)
     {
         // code here
         int count = 0;
+        int start = 0, end = arr.size() - 1, mid, fIndex = -1, lIndex = -1;
 
-        for (int i = 0; i < arr.size(); i++)
+        // code for finding first occurence
+        while (start <= end)
         {
-            if (arr[i] == target)
-                count++;
-            else if (arr[i] < target)
-                continue;
+            mid = start + (end - start) / 2;
+            if (arr[mid] == key)
+            {
+                fIndex = mid;
+                end = mid - 1;
+            }
+            else if (arr[mid] < key)
+                start = mid + 1;
             else
-                break;
+            {
+                end = mid - 1;
+            }
         }
 
-        return count;
+        start = 0, end = arr.size() - 1;
+        // code for finding last occurence
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            if (arr[mid] == key)
+            {
+                lIndex = mid;
+                start = mid + 1;
+            }
+            else if (arr[mid] > key)
+                end = mid - 1;
+            else
+            {
+                start = mid + 1;
+            }
+        }
+
+        if (fIndex == -1 && lIndex == -1)
+            return 0;
+        return lIndex - fIndex + 1;
     }
 };
-
 int main()
 {
+
+    return 0;
 }
